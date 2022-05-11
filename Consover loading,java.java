ce6811381fe22970ad7d732bab1f4689098b0099
AFTAB229 <104304367+AFTAB229@users.noreply.github.com>
2022-05-11 17:11:16 +0530

    Initial commit

---
+++ Consover loading,java.java
@@ -0,0 +1,17 @@
+public class Consover loading{
+private string name;
+private int age;
+
+punlic consover loading(string name, int age)
+this.name= name;
+this .age= age;
+}
+public consover loading (string name){
+System.out.println(This is a constructor with 2 parameters )
+this.name=name;
+}
+public static void main(string []args)
+consover loading obj1= new consover loading("aftab",23);
+consover loading obj1= new consover loading("lone",22);
+}
+}
